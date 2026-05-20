f203edf01d94c385f6c5dc47830c538bfd072d81
Alieva05 <lika.alieva111@gmail.com>
2026-05-21 00:00:57 +0300

    Добавление комментариев в код

--- hello_world.cpp
+++ hello_world.cpp
@@ -1,10 +1,11 @@
 #include <iostream>
 #include <string>
 
+// Основная функция программы
 int main() {
-    std::string name;
-    std::cout << "Enter your name: ";
-    std::cin >> name;
-    std::cout << "Hello world from " << name << "!" << std::endl;
+    std::string name;                     // Переменная для хранения имени пользователя
+    std::cout << "Enter your name: ";    // Запрос имени
+    std::cin >> name;                    // Чтение имени
+    std::cout << "Hello world from " << name << "!" << std::endl; // Приветствие
     return 0;
 }
